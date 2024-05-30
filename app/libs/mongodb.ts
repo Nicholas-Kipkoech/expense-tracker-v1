b1fe01b8d8df0b3433ba369a8124d28dad0a8326
Nicholas Kipkoech <nickey968@gmail.com>
2024-05-30 16:19:49 +0300

    fix

--- app/libs/mongodb.ts
+++ app/libs/mongodb.ts
@@ -2,18 +2,34 @@ import mongoose from "mongoose";
 
 const uri: string | any =
   "mongodb+srv://Nickey:Nickey@cluster0.i0gut.mongodb.net/expense-tracker?retryWrites=true&w=majority&appName=Cluster0";
-const options: any = {
-  useUnifiedTopology: true,
+
+const options = {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-export const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(uri, options);
-    console.log("connected to database...");
-  } catch (error) {
-    console.error(error);
-  } finally {
-    mongoose.disconnect();
+if (!uri) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
