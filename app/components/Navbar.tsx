277cacb0c418a0e108e70e7e17008beb0b56f00f
Nicholas Kipkoech <nickey968@gmail.com>
2024-05-31 10:25:37 +0300

    fix error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,10 +14,12 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 text-white bg-[#092332] flex justify-between items-center z-10  h-[6rem] ">
       <div className="h-[5rem] w-[5rem] bg-slate-200 rounded-[50%] flex items-center justify-center border m-2 text-white">
-        <p className="text-[2rem] font-bold text-black">
-          {user?.firstName[0]}
-          {user.lastName[0]}
-        </p>
+        {Object.keys(user).length > 0 && (
+          <p className="text-[2rem] font-bold text-black">
+            {user?.firstName[0]}
+            {user.lastName[0]}
+          </p>
+        )}
       </div>
     </div>
   );
