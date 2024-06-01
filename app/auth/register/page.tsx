b38995fd262fa9dd5d46e94ff7e0278045805b37
Nicholas Kipkoech <nickey968@gmail.com>
2024-06-01 10:18:33 +0300

    add registration screen

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,7 +1,115 @@
-import React from "react";
+"use client";
+
+import CustomButton from "@/app/helpers/CustomButton";
+import CustomInput from "@/app/helpers/CustomInput";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { config } from "dotenv";
+import { API_URL } from "@/app/config/config";
+import Image from "next/image";
+import Logo from "../../assets/Logo.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  /**
+   * This is function for triggering login. Once the login is success the user is redirected to dashboard
+   */
+  const handleRegister = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post(`${API_URL}/api/user/register`, {
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
+        phoneNumber: userDetails.phoneNumber,
+        email: userDetails.email.toLowerCase(),
+        password: userDetails.password,
+      });
+      if (res.data.success === true) {
+        setLoading(false);
+        router.push("/auth/login");
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <Image alt="logo" src={Logo} height={"80"} width={"200"} />
+      <p className="text-[1.4rem] font-bold justify-center flex">
+        Expense Tracker
+      </p>
+      <p className="text-[1rem] font-semibold flex justify-center">
+        Create your account
+      </p>
+      <div className="border px-4 w-full mx-10 shadow-2xl rounded-md">
+        <CustomInput
+          name={"First Name"}
+          className={"h-[3rem] border w-full rounded-md"}
+          value={userDetails.firstName}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, firstName: e.target.value })
+          }
+        />
+        <CustomInput
+          name={"Last Name"}
+          className={"h-[3rem] border w-full rounded-md"}
+          value={userDetails.lastName}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, lastName: e.target.value })
+          }
+        />
+        <CustomInput
+          name={"Phone Number"}
+          className={"h-[3rem] border w-full rounded-md"}
+          value={userDetails.phoneNumber}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, phoneNumber: e.target.value })
+          }
+        />
+        <CustomInput
+          name={"Email"}
+          className={"h-[3rem] border w-full rounded-md"}
+          value={userDetails.email}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, email: e.target.value })
+          }
+        />
+
+        <CustomInput
+          name={"Password"}
+          className={"h-[3rem] border w-full rounded-md"}
+          value={userDetails.password}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, password: e.target.value })
+          }
+        />
+        <CustomButton
+          name={loading ? "Registering..." : "Register"}
+          onClick={handleRegister}
+          className="w-full border h-[3rem] mt-5 rounded-md bg-slate-800 text-white"
+        />
+
+        <div className="flex justify-center gap-1 mt-1 items-center">
+          <p>Already have an account?</p>
+          <a href="/auth/login" className="text-blue-800">
+            Login
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
